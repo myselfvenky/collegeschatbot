fa42a85c0dd59350921c00316ff919d1890bdcba
myselfvenky <venkateshdonthula2002@gmail.com>
2022-11-04 01:05:16 +0530

    asdajsi

---
+++ main.py
@@ -0,0 +1,75 @@
+### Author: Riya Nakarmi ###
+### College Project ###
+
+import random
+import json
+import pickle
+import numpy as np
+from flask import Flask, redirect, url_for, request ,jsonify
+from flask_cors import CORS
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.python.keras.models import load_model
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbotmodel.h5')
+
+app = Flask(__name__)
+CORS(app)
+@app.route("/" , methods=["POST"])
+def home():
+    data =  request.get_json()
+    if request.method == "POST":
+        message = data["chattext"]
+        while True:
+            if message == "bye" or message == "Goodbye":
+                ints = predict_class(message)
+                res = get_response(ints, intents)
+                return jsonify({"Response" : res})
+            else:
+                ints = predict_class(message)
+                res = get_response(ints, intents)
+                return jsonify({"Response" : res})
+
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word)  for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words= clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda  x:x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+
+def get_response(intents_list,intents_json):
+    tag= intents_list[0]['intent']
+    list_of_intents =intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
